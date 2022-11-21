7048836aa109f6624079867def2c91bad5e926aa
root <root@DESKTOP-7CO9QQK.localdomain>
2022-11-21 20:14:10 +0100

    add ft_putnbr.c but doesn't work

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 15:37:24 by lgabet            #+#    #+#             */
-/*   Updated: 2022/11/21 19:45:37 by lgabet           ###   ########.fr       */
+/*   Updated: 2022/11/21 19:53:18 by lgabet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,5 +51,6 @@ void    ft_striteri(char *s, void (*f)(unsigned int, char*));
 void    ft_putchar_fd(char c, int fd);
 void    ft_putstr_fd(char *s, int fd);
 void    ft_putendl_fd(char *s, int fd);
+void    ft_putnbr_fd(int n, int fd);
 
 #endif
