d0310417cd7f86396a9113076c96c9bb4ce77c25
root <root@DESKTOP-7CO9QQK.localdomain>
2022-12-05 21:10:42 +0100

    everything compile with fsanitize=address

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 10:06:45 by lgabet            #+#    #+#             */
-/*   Updated: 2022/11/10 12:41:44 by lgabet           ###   ########.fr       */
+/*   Updated: 2022/12/05 20:58:17 by lgabet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	size_t		i;
-	char		*p;
+	size_t				i;
+	unsigned char		*p;
 
 	i = 0;
 	p = b;
 	while (i < len)
 	{
-		p[i] = c;
+		p[i] = (unsigned char)c;
 		i++;
 	}
 	return (b);
